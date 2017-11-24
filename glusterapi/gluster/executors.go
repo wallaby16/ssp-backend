0998fdf18eea636b43f9d2673292f9d691f77ea4
u220374 <reto.lehmann@sbb.ch>
2017-11-24 14:49:21 +0100

    Catch exit status 5 in lvextend

--- glusterapi/gluster/executors.go
+++ glusterapi/gluster/executors.go
@@ -3,6 +3,7 @@ package gluster
 import (
 	"errors"
 	"log"
+	"strings"
 )
 
 func executeCommandsLocally(commands []string) error {
@@ -10,8 +11,11 @@ func executeCommandsLocally(commands []string) error {
 	for _, c := range commands {
 		out, err := ExecRunner.Run("bash", "-c", c)
 		if err != nil {
-			log.Println("Error executing command: ", c, err.Error(), string(out))
-			return errors.New(commandExecutionError)
+			// If lvextend has the same size exit code 5 is fine
+			if !(strings.Contains(c, "lvextend") && strings.Contains(err.Error(), "exit status 5")) {
+				log.Println("Error executing command: ", c, err.Error(), string(out))
+				return errors.New(commandExecutionError)
+			}
 		}
 		log.Printf("Cmd: %v | StdOut: %v", c, string(out))
 	}
